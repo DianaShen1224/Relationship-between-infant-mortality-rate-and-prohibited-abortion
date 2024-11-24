f9649b342304ecbfb0a0eae22cfd6e2560241c37
DianaShen1224 <diana.shen@utoronto.ca>
2024-11-24 03:03:08 -0500

    update paper

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -11,27 +11,39 @@
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+first_model <- stan_glmer(
+  formula = death_rate ~ after_injunction * abortion_illegal + abortion_illegal + (1 | state) ,
+  data = analysis_data,
+  family = gaussian(),
+  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_aux = exponential(rate = 1, autoscale = TRUE),
+  seed = 853
+)
 
 
+second_model <- stan_glmer(
+  formula = death_rate ~ after_injunction * abortion_illegal + year_of_death +age_of_mother+ mothers_single_race + (1 | state),
+  data = analysis_data,
+  family = gaussian(),
+  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_aux = exponential(rate = 1, autoscale = TRUE),
+  seed = 853
+)
 #### Save model ####
 saveRDS(
   first_model,
   file = "models/first_model.rds"
 )
-
+saveRDS(
+  second_model,
+  file = "models/second_model.rds"
+)
 
